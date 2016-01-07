3e1a1b6d47de4e8fabb557d5f06d0395763b153f
Nick Chernyshev <nick.chernyshev@gmail.com>
2016-01-07 10:37:26 +0100

    init recipe

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name              "rails_gem_dependencies"
+maintainer        "Dwwd"
+maintainer_email  "info@dwwd.ru"
+description       "Installs packages required by rails gems"
+version           "0.0.4"
+
+recipe "rails_gem_dependencies", "Installs Gemss Prerequisites"
+
+supports "ubuntu"
+
+depends "apt"
